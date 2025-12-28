5bd6f68700c70ed8c4f39b857fdaed299e76d30d
sasa0xx <vole2019123@gmail.xom>
2025-12-28 19:01:38 +0000

    implemented matrix

--- types.go
+++ types.go
@@ -1,1 +1,23 @@
 package main
+
+type Matrix struct {
+	Rows int
+	Cols int
+	Data []float64
+}
+
+func NewMatrix(rows int, cols int) Matrix {
+	return Matrix{
+		Rows: rows,
+		Cols: cols,
+		Data: make([]float64, rows*cols),
+	}
+}
+
+func (m *Matrix) At(i int, j int) float64 {
+	return m.Data[i*m.Cols+j]
+}
+
+func (m *Matrix) Set(i int, j int, v float64) {
+	m.Data[i*m.Cols+j] = v
+}
